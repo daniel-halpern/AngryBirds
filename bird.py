06879be9f9ce8a3c7988b211a743d9d0309defb2
daniel-halpern <82973812+daniel-halpern@users.noreply.github.com>
2024-05-22 09:17:26 -0400

    Added gravity

--- bird.py
+++ bird.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 class Bird:
     def __init__(self, pos, mass):
@@ -6,8 +7,19 @@ class Bird:
         self.inSlingshot = True
         self.velocity = [0, 0]
         self.mass = mass
+        self.Fnetx = 0
+        self.Fnety = 0
+        self.accx = 0
+        self.accy = -20000
+        self.airTime = 0
+        self.startTime = 0
 
     def calculateVelocity(self, energy, theta):
         velocity = math.sqrt(2 * energy)
+        self.airTime = time.time() - self.startTime
         self.velocity[0] = -velocity * math.cos(theta)
         self.velocity[1] = velocity * math.sin(theta)
+    
+    def recalculateVelocity(self):
+        self.velocity[0] = self.velocity[0] + self.accx * self.airTime
+        self.velocity[1] = self.velocity[1] + self.accy * self.airTime
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from slingshot import *
 from player import *
 from game import *
 from helpers import *
+import time
 
 # Pygame setup
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((1400, 1000))
 clock = pygame.time.Clock()
 running = True
 
 # Game setup
-slingshot = Slingshot([200,200])
+slingshot = Slingshot([200,600])
 bird = Bird(slingshot.pos, 10)
 game = Game()
 
@@ -34,17 +35,19 @@ while running:
     # In this case, the player just let go the slingshot
     elif bird.inSlingshot and abs(slingshot.stretch) > 0:
         bird.inSlingshot = False
+        bird.startTime = time.time()
         bird.calculateVelocity(slingshot.springPotentialEnergy(), 
                                calculateAngle(slingshot.pos, bird.pos))
     # In this case, the bird already has some initial velocity
     elif bird.velocity != [0, 0]: # Bird is launched
+        bird.recalculateVelocity()
         calculateBirdPosition(slingshot, bird, game, dt)
 
 
     # Drawing
     screen.fill("aqua")
     pygame.draw.circle(screen, "blue", slingshot.pos, 50) # The slingshot size
-    pygame.draw.circle(screen, "red", bird.pos, 40) # The bird
+    pygame.draw.circle(screen, "red", bird.pos, 25) # The bird
     pygame.display.flip()
 
 pygame.quit()
